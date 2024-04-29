da1a6e1bd5ed9d9d9e38bc0535bf03327f633194
yangwonjoon <diddnjswns20@gmail.com>
2024-04-29 16:59:29 +0900

    [feat] blob revoke

--- src/hooks/product/useUploadProduct.ts
+++ src/hooks/product/useUploadProduct.ts
@@ -19,55 +19,49 @@ export const useUploadProduct = ({ productFormData, validateProductForm, setMsg,
     const user = userStore(state => state.user);
     const [showImages, setShowImages] = useState<string[]>(productFormData.productImages || []);
     const [imageFiles, setImageFiles] = useState<File[]>([]);
-    console.log(productFormData)
-
+    const [fileNames, setFileNames] = useState<string[]>([]); // 파일 이름을 저장하는 배열
 
     useEffect(() => {
         setShowImages(productFormData.productImages);
+        // 파일 이름을 추출하여 초기화합니다.
+        setFileNames(productFormData.productImages.map(url => url.split('/').pop() || ''));
     }, [productFormData.productImages]);
 
     // 이미지 추가
     const handleAddImages = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
-        const files = e.target.files;
-        if (files) {
-            const newImageFiles = Array.from(files);
-            // 기존 Blob URL 해제
-            showImages.forEach(url => url.startsWith('blob:') && URL.revokeObjectURL(url));
-
-            const newShowImages = newImageFiles.map(file => URL.createObjectURL(file));
-
-            setShowImages(prevImages => [...prevImages, ...newShowImages]);
-            setImageFiles(prevFiles => [...prevFiles, ...newImageFiles]);
-        }
+        const files = Array.from(e.target.files || []);
+        files.forEach(file => {
+            const fileName = file.name;
+            if (!fileNames.includes(fileName)) {
+                const newImageUrl = URL.createObjectURL(file);
+                setShowImages(prev => [...prev, newImageUrl]);
+                setImageFiles(prev => [...prev, file]);
+                setFileNames(prev => [...prev, fileName]);
+            }
+        });
     };
 
+    // 이미지 삭제
     const handleDeleteImage = async (index: number) => {
         const imageUrl = showImages[index];
+        const fileName = fileNames[index];
         const isBlob = imageUrl.startsWith('blob:');
-        // Blob URL 해제
         if (isBlob) {
             URL.revokeObjectURL(imageUrl);
         } else {
-            // Firebase Storage에서 이미지 삭제
             try {
                 await deleteProductImage(imageUrl);
-                // 삭제 성공 후, productFormData 업데이트
-                setProductFormData(prevState => ({
-                    ...prevState,
-                    productImages: prevState.productImages.filter(url => url !== imageUrl)
-                }));
             } catch (error) {
                 console.error('Error deleting image', error);
                 alert('이미지 삭제 에러');
                 return;
             }
         }
-        // showImages 및 imageFiles에서 해당 인덱스의 항목 제거
         setShowImages(prev => prev.filter((_, idx) => idx !== index));
         setImageFiles(prev => prev.filter((_, idx) => idx !== index));
+        setFileNames(prev => prev.filter((_, idx) => idx !== index));
     };
-
 
     // 폼 전송 및 업로드
     const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
@@ -75,18 +69,22 @@ export const useUploadProduct = ({ productFormData, validateProductForm, setMsg,
         if (!validateProductForm()) {
             return;
         }
-
-        const INPUT_IMG_LENGTH = showImages.length
-        if (INPUT_IMG_LENGTH < 4) {
-            setMsg('이미지는 최소 4개 이상 업로드해야 합니다.');
+        if (showImages.length < 4 || new Set(fileNames).size < 4) {
+            setMsg('이미지는 최소 4개 이상의 유니크한 파일이어야 합니다.');
             return;
         }
-
         try {
             const imageUrls = await Promise.all(imageFiles.map(file => uploadProductImageFiles(file, user.email, productFormData.productName)));
             const finalImageUrls = [...imageUrls, ...productFormData.productImages.filter(url => showImages.includes(url))];
             const finalProductFormData: IProductFormData = { ...productFormData, productImages: finalImageUrls as string[] };
             await uploadProductFormData(finalProductFormData, productFormData.productId as string);
+
+            // Blob URL 해제
+            imageFiles.forEach(file => {
+                if (file instanceof Blob) {
+                    URL.revokeObjectURL(URL.createObjectURL(file));
+                }
+            });
 
             alert('업로드 성공');
             navigate('/sell');
