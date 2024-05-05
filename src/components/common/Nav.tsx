f2383ab7f7434988e06001c3d154270871ff8083
yangwonjoon <diddnjswns20@gmail.com>
2024-05-05 20:03:25 +0900

    [feat] nav border(url에 따라)

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,10 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { userStore } from "@/store/userStore";
+import { useEffect, useState } from "react";
 
 export const Nav = () => {
+    const navigate = useNavigate();
+    const location = useLocation()
+    const { user } = userStore();
+    const [line, setLine] = useState(location.pathname);
 
-    const navigate = useNavigate()
-    const { user } = userStore()
+    useEffect(() => { setLine(location.pathname); }, [location.pathname])
 
     const clickHandle = (path: string) => {
         if (path === '/sell' && !user.isSeller) {
@@ -13,21 +17,32 @@ export const Nav = () => {
         }
         if (path === '/mypage' && !user.uid) {
             alert('로그인 해주세요!');
-            navigate('/login')
+            navigate('/login');
             return;
         }
+        navigate(path);
+    }
 
-        navigate(path)
+    const isActive = (path: string) => {
+        return line === path ? 'border-b-4 border-black' : '';
     }
 
     return (
         <>
             <div className="flex mb-5">
-                <div className={`cursor-pointer flex-1 text-center border-b-4 border-black`} onClick={() => clickHandle('/')}><h1 className="text-2xl">HOME</h1></div>
-                <div className={`cursor-pointer flex-1 text-center `} onClick={() => clickHandle('/team')}><h1 className="text-2xl">TEAM</h1></div>
-                <div className={`cursor-pointer flex-1 text-center `} onClick={() => clickHandle('/sell')} ><h1 className="text-2xl">SELL</h1></div>
-                <div className={`cursor-pointer flex-1 text-center `} onClick={() => clickHandle('/mypage')}><h1 className="text-2xl">MYPAGE</h1></div>
-            </div >
+                <div className={`cursor-pointer flex-1 text-center ${isActive('/')}`} onClick={() => clickHandle('/')}>
+                    <h1 className="text-2xl">HOME</h1>
+                </div>
+                <div className={`cursor-pointer flex-1 text-center ${isActive('/team')}`} onClick={() => clickHandle('/team')}>
+                    <h1 className="text-2xl">TEAM</h1>
+                </div>
+                <div className={`cursor-pointer flex-1 text-center ${isActive('/sell')}`} onClick={() => clickHandle('/sell')}>
+                    <h1 className="text-2xl">SELL</h1>
+                </div>
+                <div className={`cursor-pointer flex-1 text-center ${isActive('/mypage')}`} onClick={() => clickHandle('/mypage')}>
+                    <h1 className="text-2xl">MYPAGE</h1>
+                </div>
+            </div>
         </>
-    )
+    );
 }
