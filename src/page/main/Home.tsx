3648740122e49b5efcd9fefdb6a3592c91e27d9f
yangwonjoon <diddnjswns20@gmail.com>
2024-05-05 19:54:53 +0900

    [fix] event bubbling 제거

--- src/page/main/Home.tsx
+++ src/page/main/Home.tsx
@@ -8,9 +8,11 @@ import { db } from "@/firebase";
 import { IProductFormData } from "@/types/types";
 import { cartStore } from "@/store/cartStore";
 import { userStore } from "@/store/userStore";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
     const [products, setProducts] = useState<IProductFormData[]>([]);
+    const navigate = useNavigate()
     const { cart, addCart, removeCart } = cartStore()
     const { user } = userStore()
 
@@ -33,7 +35,8 @@ export const Home = () => {
         readProducts();
     }, []);
 
-    const handleToggleCart = (productId: string | undefined) => {
+    const handleToggleCart = (productId: string | undefined, e: React.MouseEvent) => {
+        e.stopPropagation();
         if (!productId) {
             alert('제품 정보가 올바르지 않습니다.');
             return;
@@ -56,13 +59,13 @@ export const Home = () => {
                 <div className="grid grid-cols-3 gap-4">
                     {products && (
                         products.map((product, i) => (
-                            <div key={i} className="mx-auto mb-3 w-48 h-72 overflow-hidden">
+                            <div key={i} className="mx-auto mb-3 w-48 h-72 overflow-hidden" onClick={() => navigate(`/${product.productId}`)}>
                                 <div className="flex items-center justify-center h-48 shadow-lg rounded-lg border border-black relative">
                                     <img src={product.productImages[0]} alt="메인 이미지" className="object-contain h-full" />
                                     {
                                         product.productId && cart.includes(product.productId) ?
-                                            <FontAwesomeIcon icon={filledBookmark} className="absolute bottom-0 right-0 m-2" onClick={() => handleToggleCart(product.productId)} />
-                                            : <FontAwesomeIcon icon={bookmark} className="absolute bottom-0 right-0 m-2" onClick={() => handleToggleCart(product.productId)} />
+                                            <FontAwesomeIcon icon={filledBookmark} className="absolute bottom-0 right-0 m-2" onClick={(e) => handleToggleCart(product.productId, e)} />
+                                            : <FontAwesomeIcon icon={bookmark} className="absolute bottom-0 right-0 m-2" onClick={(e) => handleToggleCart(product.productId, e)} />
                                     }
                                 </div>
                                 <div className="flex flex-col p-3">
