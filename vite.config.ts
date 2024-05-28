f3417e5462f1b9f68445928b4f6f68332b958d69
yang <diddnjswns20@gmail.com>
2024-05-28 16:42:29 +0900

    [setting] eslint, prettier setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/'),
+      '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 })
