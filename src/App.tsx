8458d294b444a27bffe446df559f6d5070826a63
yang <diddnjswns20@gmail.com>
2024-06-02 04:33:05 +0900

    [feat] Apply lazy loading to routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Home from '@/page/product/Home'
-import Team from '@/page/product/Team'
-import ProductDetail from '@/page/product/ProductDetail'
 import existedCartState from '@/api/cart/existedCartState'
 import Layout from '@/routes/Layout'
 import AuthLayout from '@/routes/AuthLayout'
+import Loading from './components/common/Loading'
 
+const Home = lazy(() => import('@/page/product/Home'))
+const Team = lazy(() => import('@/page/product/Team'))
+const ProductDetail = lazy(() => import('@/page/product/ProductDetail'))
 const SignUp = lazy(() => import('@/page/signup/SignUp'))
 const SellerSignUp = lazy(() => import('@/page/signup/SellerSignUp'))
 const CustomerSignUp = lazy(() => import('@/page/signup/CustomerSignUp'))
@@ -25,7 +26,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
